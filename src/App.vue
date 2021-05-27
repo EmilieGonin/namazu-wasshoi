96e6a272061a23440317c418791ebb77feb78cdd
EmilieGonin <emilie.yuu@gmail.com>
2021-05-27 23:31:02 +0200

    feat: add loading spinner design & animation

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
+  <AppLoading />
   <AppNav />
   <router-view />
 </template>
 
 <script>
+import AppLoading from "@/components/AppLoading.vue";
 import AppNav from "@/components/AppNav.vue";
 
 export default {
   components: {
+    AppLoading,
     AppNav
   }
 };
---
+++ src/components/AppLoading.vue
@@ -0,0 +1,46 @@
+<template lang="html">
+  <div class="loading" v-if="loading">
+    <div class="loading__ball loading__ball--mog"></div>
+    <div class="loading__ball loading__ball--chocobo"></div>
+    <div class="loading__ball loading__ball--pampa"></div>
+    <div class="loading__ball loading__ball--carbuncle"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppLoading",
+  data() {
+    return {
+      loading: false
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+.loading {
+  @include absolute-center;
+  @include flex;
+  z-index: 999;
+  width: 100%;
+  background: fade-out(white, 0.5);
+  &__ball {
+    margin: 1px;
+    width: 27px;
+    height: 27px;
+    border: 2px solid $grey;
+    border-radius: 50%;
+    box-shadow: -1px -1px 5px $lightblack inset;
+
+    @each $team, $color in $teams {
+      $i: index(($teams), ($team $color));
+      &--#{$team} {
+        background: $color;
+        animation: $load-dur blink ease-in-out infinite alternate;
+        animation-delay: $load-delay * $i;
+      }
+    }
+  }
+}
+</style>
