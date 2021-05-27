56511acf58a7f14e689c8e2870b69576000abc3e
EmilieGonin <emilie.yuu@gmail.com>
2021-05-27 16:23:41 +0200

    build(deps): add font-awesome

--- src/main.js
+++ src/main.js
@@ -4,4 +4,9 @@ import router from './router'
 import store from './store'
 import "@/stylesheets/global.scss"
 
-createApp(App).use(store).use(router).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add()
+
+createApp(App).use(store).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app')
