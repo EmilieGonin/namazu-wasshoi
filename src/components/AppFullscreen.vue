c4ab9610e8d06e7bb75ecba2bcda044cb72ca860
EmilieGonin <emilie.yuu@gmail.com>
2021-05-30 16:47:12 +0200

    feat: add Fullscreen page model

---
+++ src/components/AppFullscreen.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="fullscreen">
+    <div class="fullscreen__container">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppFullscreen"
+};
+</script>
+
+<style lang="scss" scoped>
+.fullscreen {
+  @include absolute-center;
+  backdrop-filter: blur(5px);
+  &__container {
+    @include absolute-center;
+    width: 35%;
+    min-width: 320px;
+    height: 50%;
+    min-height: 420px;
+    background: $minor-white;
+  }
+}
+</style>
