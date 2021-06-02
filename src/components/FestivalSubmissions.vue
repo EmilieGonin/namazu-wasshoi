f0c8457e437ea3f4656ac367b3ded2024bec48e8
EmilieGonin <emilie.yuu@gmail.com>
2021-06-03 00:30:44 +0200

    feat: add submissions selected style

--- src/components/FestivalSubmissions.vue
+++ src/components/FestivalSubmissions.vue
@@ -1,11 +1,23 @@
 <template lang="html">
   <div class="submissions">
-    <img class="submissions__img" src="@/assets/sample.png" alt="" />
-    <img class="submissions__img" src="@/assets/sample.png" alt="" />
-    <img class="submissions__img" src="@/assets/sample.png" alt="" />
-    <img class="submissions__img" src="@/assets/sample.png" alt="" />
-    <img class="submissions__img" src="@/assets/sample.png" alt="" />
-    <img class="submissions__img" src="@/assets/sample.png" alt="" />
+    <div class="submissions__container submissions__container--selected">
+      <img class="submissions__img" src="@/assets/sample.png" alt="" />
+    </div>
+    <div class="submissions__container">
+      <img class="submissions__img" src="@/assets/sample.png" alt="" />
+    </div>
+    <div class="submissions__container">
+      <img class="submissions__img" src="@/assets/sample.png" alt="" />
+    </div>
+    <div class="submissions__container">
+      <img class="submissions__img" src="@/assets/sample.png" alt="" />
+    </div>
+    <div class="submissions__container">
+      <img class="submissions__img" src="@/assets/sample.png" alt="" />
+    </div>
+    <div class="submissions__container">
+      <img class="submissions__img" src="@/assets/sample.png" alt="" />
+    </div>
   </div>
 </template>
 
@@ -13,22 +25,35 @@
 // import AppButton from "@/components/AppButton.vue";
 
 export default {
-  name: "FestivalSubmissions"
+  name: "FestivalSubmissions",
+  data() {
+    return {
+      selected: ""
+    };
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .submissions {
-  @include flex($gap: 10);
+  @include flex($gap: 5);
   flex-wrap: wrap;
   margin-bottom: 15px;
+  &__container {
+    border: 3px solid white;
+    &--selected {
+      border: 3px solid veil($valid);
+      border-radius: 5px;
+    }
+  }
   &__img {
+    display: block;
     cursor: pointer;
     width: 200px;
-    height: 230px;
+    height: 220px;
     object-fit: cover;
     border: 5px solid white;
-    border-bottom: 50px solid white;
+    border-bottom: 40px solid white;
     box-shadow: 0 0 2px $main-black;
   }
 }
