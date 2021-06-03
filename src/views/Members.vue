ea9fcb1a885f3ef0669ff2c3a55ac3ed4ec835e7
EmilieGonin <emilie.yuu@gmail.com>
2021-06-03 14:57:58 +0200

    feat: add members list style + current member name as title

--- src/views/Members.vue
+++ src/views/Members.vue
@@ -1,7 +1,19 @@
 <template lang="html">
   <div class="members" v-if="status && status != 'loading'">
-    <div v-for="member in fcMembers" :key="member.ID">
-      <img :src="member.Avatar" alt="Avatar" :title="member.Name" />
+    <div class="members__name">
+      {{ currentMember }}
+    </div>
+    <div class="members__container">
+      <div v-for="member in fcMembers" :key="member.ID">
+        <img
+          class="members__avatar"
+          :src="member.Avatar"
+          alt="Avatar"
+          :title="member.Name"
+          @mouseover="setCurrentMember"
+          @mouseleave="reset"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -11,13 +23,52 @@ import { mapGetters } from "vuex";
 
 export default {
   name: "Members",
+  data() {
+    return {
+      currentMember: ""
+    };
+  },
   computed: {
-    ...mapGetters(["status", "fc", "fcMembers"])
+    ...mapGetters(["status", "fcMembers"])
   },
   mounted() {
     this.$store.dispatch("setFreeCompany");
+  },
+  methods: {
+    setCurrentMember(e) {
+      this.currentMember = e.target.title;
+    },
+    reset() {
+      this.currentMember = "";
+    }
   }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.members {
+  @include flex($direction: column);
+  width: 100%;
+  max-width: 800px;
+  margin: auto;
+  &__name {
+    @include cursive(50);
+    margin: 20px 0;
+    height: 69px;
+  }
+  &__container {
+    @include flex($gap: 5);
+    flex-wrap: wrap;
+  }
+  &__avatar {
+    cursor: pointer;
+    width: 70px;
+    border-radius: 25%;
+    border: 3px solid $grey;
+    box-shadow: 0 0 2px $main-black;
+    &:hover {
+      border: 3px solid $namazu;
+    }
+  }
+}
+</style>
