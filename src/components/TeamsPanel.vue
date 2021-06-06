fd1d8ae0dedcc5bd25e98683636936846d3c46a4
EmilieGonin <emilie.yuu@gmail.com>
2021-06-06 12:08:26 +0200

    feat: add Teams component with TeamsPanel

---
+++ src/components/TeamsPanel.vue
@@ -0,0 +1,108 @@
+<template lang="html">
+  <div class="panel">
+    <div class="panel__img-container">
+      <img
+        class="panel__img"
+        :class="`panel__img--${team}`"
+        src="@/assets/sample.png"
+        alt=""
+      />
+      <div class="panel__title-container">
+        <div class="panel__title" :class="`panel__title--${team}`">
+          {{ team }}
+        </div>
+        <div class="panel__subtitle"><slot></slot></div>
+      </div>
+    </div>
+    <div class="panel__background" :class="`panel__background--${team}`"></div>
+    <div class="panel__container">
+      (Liste des membres)
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TeamsPanel",
+  props: {
+    team: String
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.panel {
+  @include flex($direction: column);
+  position: relative;
+  width: 100%;
+  margin-top: 40px;
+  border-bottom: 30px solid white;
+  &__img-container {
+    position: relative;
+    width: 90%;
+  }
+  &__img {
+    display: block;
+    width: 100%;
+    height: 500px;
+    object-fit: cover;
+    box-shadow: 0 0 1px $main-black;
+
+    @each $team, $color in $teams {
+      &--#{$team} {
+        border: 5px solid pastel($color);
+      }
+    }
+  }
+  &__title-container {
+    position: absolute;
+    bottom: 7px;
+    left: 5px;
+    right: 5px;
+    margin: auto;
+    text-align: center;
+    background: veil(black, 0.7);
+  }
+  &__title {
+    @include cursive(50);
+    text-transform: capitalize;
+
+    @each $team, $color in $teams {
+      &--#{$team} {
+        @include font-relief($color);
+      }
+    }
+  }
+  &__subtitle {
+    transform: translateY(-7px);
+    color: white;
+    &::before {
+      content: "« ";
+    }
+    &::after {
+      content: " »";
+    }
+  }
+  &__background {
+    z-index: -1;
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 70%;
+
+    @each $team, $color in $teams {
+      &--#{$team} {
+        background: $color;
+      }
+    }
+  }
+  &__container {
+    width: 100%;
+    max-width: 1000px;
+    padding: 50px;
+    padding-bottom: 20px;
+    background: white;
+    text-align: justify;
+  }
+}
+</style>
--- src/views/Teams.vue
+++ src/views/Teams.vue
@@ -1,10 +1,22 @@
 <template lang="html">
-  <div class="teams"></div>
+  <div class="teams">
+    <TeamsPanel :team="'mog'">Pas de coups sous le pompon !</TeamsPanel>
+    <TeamsPanel :team="'chocobo'">On est chaud... cobo !</TeamsPanel>
+    <TeamsPanel :team="'pampa'">Qui s'y frotte s'y pique !</TeamsPanel>
+    <TeamsPanel :team="'carbuncle'">
+      Que la poussière de diamant vous réduise en éclat !
+    </TeamsPanel>
+  </div>
 </template>
 
 <script>
+import TeamsPanel from "@/components/TeamsPanel.vue";
+
 export default {
-  name: "Teams"
+  name: "Teams",
+  components: {
+    TeamsPanel
+  }
 };
 </script>
 
