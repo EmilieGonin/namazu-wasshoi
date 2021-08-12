ede20fa8e7aeea341f110d24e5da04659d212f04
EmilieGonin <emilie.yuu@gmail.com>
2021-08-12 13:38:30 +0200

    feat: use WebHistory instead of WebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import store from '../store'
 
 const routes = [
@@ -125,7 +125,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior() {
     // always scroll to top
