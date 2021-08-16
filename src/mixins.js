e89b6cd9f91494628b389ab77395a7d7ff9fc96a
EmilieGonin <emilie.yuu@gmail.com>
2021-08-16 18:12:00 +0200

    feat: improve code + prevent sending empty email or password

--- src/mixins.js
+++ src/mixins.js
@@ -5,6 +5,7 @@ const formValidate = {
       const datas = JSON.parse(JSON.stringify(this.$data));
 
       for (const data of Object.entries(datas)) {
+        //Get all datas starting with form_ or formo_
         if (data[0].startsWith("form_") || data[0].startsWith("formo_")) {
           const name = data[0].split("_")[1];
           const value = data[1];
@@ -14,32 +15,34 @@ const formValidate = {
               "Veuillez renseigner tous les champs requis du formulaire.";
             this.$store.dispatch("error", error);
             throw error;
-          } else {
+          } else if (value) {
             form[name] = value;
 
             if (
-              data[0].includes("email") &&
-              !data[1].match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+              name == "email" &&
+              !value.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
             ) {
               const error = "L'adresse email est invalide.";
               this.$store.dispatch("error", error);
               throw error;
             } else if (
-              data[0].includes("password") &&
-              !data[1].match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
+              name == "password" &&
+              !value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
             ) {
               const error =
                 "Le mot de passe doit contenir au moins 8 caractères dont une minuscule, une majuscule et un chiffre.";
               this.$store.dispatch("error", error);
               throw error;
             } else if (
-              data[0].includes("discord") &&
-              !data[1].match(/^[a-zA-Z0-9._%+-]+#[0-9]+$/)
+              name == "discord" &&
+              !value.match(/^[a-zA-Z0-9._%+-]+#[0-9]+$/)
             ) {
               const error = "L'identifiant Discord est incorrect.";
               this.$store.dispatch("error", error);
               throw error;
             }
+          } else if (!value && name != "email" && name != "password") {
+            form[name] = null;
           }
         }
       }
