8237107944375e10b2b04e7c28a982eb8d65920a
EmilieGonin <emilie.yuu@gmail.com>
2021-08-16 14:45:58 +0200

    feat: change MiniSpinner default color + computed style with color prop

--- src/components/AppMiniSpinner.vue
+++ src/components/AppMiniSpinner.vue
@@ -2,18 +2,25 @@
   <font-awesome-icon
     class="fa-spin mini-spinner"
     :icon="['fad', 'spinner-third']"
+    :style="style"
     fixed-width
   />
 </template>
 
 <script>
 export default {
-  name: "AppMiniSpinner"
+  name: "AppMiniSpinner",
+  props: {
+    color: String
+  },
+  computed: {
+    style() {
+      return {
+        color: this.color ? this.color : "#b8b6c1"
+      };
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped>
-.mini-spinner {
-  color: white;
-}
-</style>
+<style lang="scss" scoped></style>
