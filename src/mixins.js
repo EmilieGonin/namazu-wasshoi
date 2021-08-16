c6c3b802d6a2ea57125639b74987982c575fae9e
EmilieGonin <emilie.yuu@gmail.com>
2021-08-16 17:18:29 +0200

    fix: add value to form if not empty + don't send regex error if no value

--- src/mixins.js
+++ src/mixins.js
@@ -8,37 +8,38 @@ const formValidate = {
         if (data[0].startsWith("form_") || data[0].startsWith("formo_")) {
           const name = data[0].split("_")[1];
           const value = data[1];
-          form[name] = value;
 
           if (!data[0].startsWith("formo_") && !value) {
             const error =
               "Veuillez renseigner tous les champs requis du formulaire.";
             this.$store.dispatch("error", error);
             throw error;
-          }
-
-          if (
-            data[0].includes("email") &&
-            !data[1].match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
-          ) {
-            const error = "L'adresse email est invalide.";
-            this.$store.dispatch("error", error);
-            throw error;
-          } else if (
-            data[0].includes("password") &&
-            !data[1].match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
-          ) {
-            const error =
-              "Le mot de passe doit contenir au moins 8 caractères dont une minuscule, une majuscule et un chiffre.";
-            this.$store.dispatch("error", error);
-            throw error;
-          } else if (
-            data[0].includes("discord") &&
-            !data[1].match(/^[a-zA-Z0-9._%+-]+#[0-9]+$/)
-          ) {
-            const error = "L'identifiant Discord est incorrect.";
-            this.$store.dispatch("error", error);
-            throw error;
+          } else if (value) {
+            form[name] = value;
+            
+            if (
+              data[0].includes("email") &&
+              !data[1].match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+            ) {
+              const error = "L'adresse email est invalide.";
+              this.$store.dispatch("error", error);
+              throw error;
+            } else if (
+              data[0].includes("password") &&
+              !data[1].match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)
+            ) {
+              const error =
+                "Le mot de passe doit contenir au moins 8 caractères dont une minuscule, une majuscule et un chiffre.";
+              this.$store.dispatch("error", error);
+              throw error;
+            } else if (
+              data[0].includes("discord") &&
+              !data[1].match(/^[a-zA-Z0-9._%+-]+#[0-9]+$/)
+            ) {
+              const error = "L'identifiant Discord est incorrect.";
+              this.$store.dispatch("error", error);
+              throw error;
+            }
           }
         }
       }
