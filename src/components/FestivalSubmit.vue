a0eee530e368a43bad986488a48ca0f67ba1a83a
EmilieGonin <emilie.yuu@gmail.com>
2021-08-18 16:50:27 +0200

    feat: add FestivalSubmit component

---
+++ src/components/FestivalSubmit.vue
@@ -0,0 +1,13 @@
+<template lang="html">
+  <div class="submit">
+    Test
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FestivalSubmit"
+};
+</script>
+
+<style lang="scss" scoped></style>
