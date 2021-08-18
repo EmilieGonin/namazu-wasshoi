0a44028983509ebff064e7c15dfa977ff1ca104f
EmilieGonin <emilie.yuu@gmail.com>
2021-08-18 17:23:30 +0200

    feat: add simple file input and form button

--- src/components/FestivalSubmit.vue
+++ src/components/FestivalSubmit.vue
@@ -1,13 +1,60 @@
 <template lang="html">
   <div class="submit">
-    Test
+    <form
+      class="form"
+      @submit.prevent="submit"
+      @keyup.enter="submit"
+      method="post"
+    >
+      <FormElement
+        v-model="form_file"
+        :label="'Choisissez votre screenshot'"
+        :name="'form_file'"
+        :type="'file'"
+        :required="true"
+      ></FormElement>
+      <AppButton @click="submit">Valider la participation</AppButton>
+    </form>
   </div>
 </template>
 
 <script>
+import { formValidate } from "@/mixins.js";
+import FormElement from "@/components/FormElement.vue";
+import AppButton from "@/components/AppButton.vue";
+
 export default {
-  name: "FestivalSubmit"
+  name: "FestivalSubmit",
+  components: {
+    FormElement,
+    AppButton
+  },
+  data() {
+    return {
+      form_file: ""
+    };
+  },
+  mixins: [formValidate],
+  methods: {
+    submit() {
+      try {
+        const form = this.formValidate();
+        console.log(form);
+        // this.$store.dispatch("submitScreenshot", form).catch(e => {
+        //   console.error(e);
+        // });
+      } catch (e) {
+        console.error(e);
+      }
+    }
+  }
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.submit {
+  border-radius: 25px;
+  border: 3px dashed $namazu;
+  padding: 50px;
+}
+</style>
