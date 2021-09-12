781bae9ca1c14fa327d61fa0af8fe41ac1117953
EmilieGonin <emilie.yuu@gmail.com>
2021-09-12 19:42:53 +0200

    feat: add AppPopup component

---
+++ src/components/AppPopup.vue
@@ -0,0 +1,45 @@
+<template lang="html">
+  <div class="popup">
+    <div class="popup__background" @click="$emit('close')"></div>
+    <div class="popup__content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppPopup",
+  emits: ["close"]
+};
+</script>
+
+<style lang="scss" scoped>
+.popup {
+  @include absolute-center;
+  position: fixed;
+  z-index: 10;
+  width: 100%;
+  background: fade-out(white, 0.5);
+  &__background {
+    @include absolute-center;
+    position: fixed;
+    z-index: 10;
+  }
+  &__content {
+    z-index: 11;
+    position: fixed;
+    top: 50%;
+    left: 0;
+    right: 0;
+    transform: translateY(-50%);
+    margin: auto;
+    padding: 50px;
+    max-width: 500px;
+    padding: 20px;
+    border-radius: 25px;
+    border: 3px dotted $minor-white;
+    background-color: white;
+  }
+}
+</style>
