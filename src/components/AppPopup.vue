e05813dc23e05bd7d4b34d8e5ec4d74f0e95eaed
EmilieGonin <emilie.yuu@gmail.com>
2021-09-13 18:48:50 +0200

    style: improve AppPopup responsive style

--- src/components/AppPopup.vue
+++ src/components/AppPopup.vue
@@ -16,31 +16,34 @@ export default {
 
 <style lang="scss" scoped>
 .popup {
+  @include responsive(425) {
+    position: absolute;
+  }
   @include absolute-center;
   position: fixed;
   z-index: 10;
   width: 100%;
-  background: fade-out(white, 0.5);
   &__background {
     @include absolute-center;
     position: fixed;
     z-index: 10;
+    background: fade-out(white, 0.5);
   }
   &__content {
     @include responsive(425) {
+      position: absolute;
       border-radius: 0;
       border-left: none;
       border-right: none;
     }
     z-index: 11;
     position: fixed;
-    top: 50%;
     left: 0;
     right: 0;
-    transform: translateY(-50%);
+    transform: translateY(70px);
     margin: auto;
     padding: 50px;
-    max-width: 500px;
+    max-width: 800px;
     padding: 20px;
     border-radius: 25px;
     border: 3px dotted $minor-white;
